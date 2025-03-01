e68fa341d65004c577fa9e530d4d17c331826b54
kinematician30 <ojoayookuntade@gmail.com>
2025-03-01 22:23:27 +0100

    add it

---
+++ hotels.py
@@ -0,0 +1,24 @@
+import requests # make a request to retrieve the page
+from bs4 import BeautifulSoup # html parser
+import pandas as pd
+
+base_url = "https://hotels.ng/hotels-in-abia"
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"}
+
+print("Connecting..")
+response_page = requests.get(base_url, headers=header)
+
+if response_page.status_code == 200:
+    print("Connected Successfully")
+else:
+    print("Connection not succesful!")
+
+# To parse the html page...
+parsed_page = BeautifulSoup(response_page.text, 'html.parser')
+
+target = parsed_page.find('div', id='topPicks')
+hotel_listing = target.find_all('div', class_="listing-hotels")
+
+for listing in hotel_listing:
+    hotel_name = listing.find('h2', class_="listing-hotels-name").text
+    print(hotel_name)
