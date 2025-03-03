c1f9f5904e669e2017a057439f145c0917fb1b85
kinematician30 <ojoayookuntade@gmail.com>
2025-03-03 07:23:02 +0100

    just added extras

--- hotels.py
+++ hotels.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup # html parser
 import pandas as pd
 
 base_url = "https://hotels.ng/hotels-in-abia"
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"}
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36q (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"}
 
 print("Connecting..")
 response_page = requests.get(base_url, headers=header)
@@ -11,13 +11,12 @@ response_page = requests.get(base_url, headers=header)
 if response_page.status_code == 200:
     print("Connected Successfully")
 else:
-    print("Connection not succesful!")
+    print("Connection not successful!")
 
 # To parse the html page...
 parsed_page = BeautifulSoup(response_page.text, 'html.parser')
 
-target = parsed_page.find('div', id='topPicks')
-hotel_listing = target.find_all('div', class_="listing-hotels")
+hotel_listing = parsed_page.find_all('div', class_="listing-hotels")
 
 for listing in hotel_listing:
     hotel_name = listing.find('h2', class_="listing-hotels-name").text
